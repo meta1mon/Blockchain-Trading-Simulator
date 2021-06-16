0d65655cd022272eebe35e2a7dca8976a3735ebf
smk <beonlyone0405@gmail.com>
2021-06-16 19:57:07 +0900

    뉴스 목록 페이징 버튼 기능 추가

--- src/main/webapp/WEB-INF/views/news/newsList.jsp
+++ src/main/webapp/WEB-INF/views/news/newsList.jsp
@@ -6,12 +6,17 @@
 <head>
 <meta charset="UTF-8">
 <title>뉴스 - BTS</title>
+<link href="${pageContext.request.contextPath}/resources/css/reset.css" rel="stylesheet" type="text/css" />
+<link href="${pageContext.request.contextPath}/resources/css/newsList.css" rel="stylesheet" type="text/css" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <jsp:include page="../main/header.jsp"></jsp:include>
 <body>
 	<div class="wrapper">
+		<input type="text" name="keyword"> 
+		<input type="submit" value="검색">
 		<div class="headLine">
+			<marquee class="scroll" onmouseover="this.stop();" onmouseout="this.start();">
 				<!-- newsList.js의 newsHeadLine() 함수 참조 -->
 			</marquee>
 		</div>
@@ -24,14 +29,15 @@
 		<div class="newsPage">
 			<a href="#">이전</a>
 			<a class="pageActive" href="#">1</a>
-			<a href="#">2</a>
-			<a href="#">3</a>
-			<a href="#">4</a>
-			<a href="#">5</a>
-			<a href="#">6</a>
+			<a class="inactive" href="#">2</a>
+			<a class="inactive" href="#">3</a>
+			<a class="inactive" href="#">4</a>
+			<a class="inactive" href="#">5</a>
+			<a class="inactive" href="#">6</a>
 			<a href="#">다음</a>
 		</div>
 	</div>
+	<script type="text/javascript"	src="${pageContext.request.contextPath}/resources/js/newsList.js"></script>
 </body>
 <jsp:include page="../main/footer.jsp"></jsp:include>
 </html>
--- src/main/webapp/resources/js/newsList.js
+++ src/main/webapp/resources/js/newsList.js
@@ -38,10 +38,8 @@ function newsHeadLine() {
 			});
 };
 
-function newsList() {
+function newsList(p) {
 	console.log("newsList 실행")
-	var p = $(".pageActive").val();
-	console.log(p);
 	$.ajax({
 				url : 'https://www.cryptohub.or.kr/api/v1/news',
 				type : "POST",
@@ -50,20 +48,39 @@ function newsList() {
 					token : "$2y$10$hUvFjpPSDU5Gx6vp20vhGOg6Nuib3IZBzZk4cR5f.uGbRtMKN.S2m"
 				},
 				success : function(data) {
-					
 					// 뉴스목록 
-					for (var i = 0; i < 3; i++) {
+					for (var i = p; i < p+3; i++) {
 						var newsTime = dateDiffer(data.data[i].pubdate);
 						$("#raw1").append('<div class="newsLabel"><img class="newsThumbnail" src="'+data.data[i].thumbnail+'"><div id="newsTitle">'+data.data[i].title+'</div><div id="newsTime">&#128344;'+newsTime+'</div></div>');
 					}
-					for (var i = 3; i < 6; i++) {
+					for (var i = p+3; i < p+6; i++) {
 						var newsTime = dateDiffer(data.data[i].pubdate);
 						$("#raw2").append('<div class="newsLabel"><img class="newsThumbnail" src="'+data.data[i].thumbnail+'"><div id="newsTitle">'+data.data[i].title+'</div><div id="newsTime">&#128344;'+newsTime+'</div></div>');
 					}
+					console.log("pubdate"+data.data[10].pubdate);  // pubdate 문제 해결해야함(temp 영상보고 다시해보기)
 				}
 			});
 };
 
+$('.newsPage a').click(function(){
+	// 버튼 색 바꿔주기
+	if($('.newsPage a').hasClass("pageActive")) {
+		$('.newsPage a').removeClass();
+		$('.newsPage a').addClass("inactive");
+		$(this).addClass("pageActive");
+	} 
+	
+	// 페이징
+	p = $(this).html();
+	p = parseInt(p)-1;// 현재 페이지 읽어온 뒤 1 빼주기
+	p *= 6;				// 6을 곱하면 상단 첫 기사 index
+	$("#raw1").empty(); // 각 클래스 내용 비워주기
+	$("#raw2").empty();
+	newsList(p);
+});
+
 newsHeadLine();
 
-newsList();
+var p = 0;
+
+newsList(p);

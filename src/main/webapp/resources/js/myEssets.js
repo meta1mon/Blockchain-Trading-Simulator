f457a5ac82302b7f5cbf88db698bae6584c8b665
smk <beonlyone0405@gmail.com>
2021-07-03 14:41:51 +0900

    코인 보유액, 총 보유자산 el태그에서 ajax로 작동되도록 변경

--- src/main/webapp/resources/js/myEssets.js
+++ src/main/webapp/resources/js/myEssets.js
@@ -14,34 +14,33 @@ function coinValue() {
 				cache : false,
 				datatype : "json",
 				success : function(data) {
+					var totalcoin = 0;
 					for (var i = 0; i < $('.coinName').length; i++) {
 						var coinName = $('.coinName').eq(i).html();
 						var coinCount = $('.nCoincnt').eq(i).val();
 						var coinValue = parseInt(data['data'][coinName]['closing_price']
 								* coinCount);
-						var nCoinValue = coinValue.toString().replace(
-								/\B(?=(\d{3})+(?!\d))/g, ","); // 세자리수
-						// 마다
-						// 콤마
-						// 표시하는
-						// 정규식
-
+						var nCoinValue = coinValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); // 세자리수 콤마
+						
 						$('.coinValue').eq(i).html(nCoinValue + "&nbsp;원");
 						var buyAvg = parseInt($('.buyAvg').eq(i).val());
 
 						var coinProfit = coinValue - buyAvg;
 						var coinProfitPcnt = (coinValue / buyAvg - 1) * 100;
 						var nCoinProfit = coinProfit.toString().replace(
-								/\B(?=(\d{3})+(?!\d))/g, ","); // 세자리수
-						// 마다
-						// 콤마
-						// 표시하는
-						// 정규식
+								/\B(?=(\d{3})+(?!\d))/g, ","); // 세자리수 콤마
+						
 						$('.coinProfit').eq(i).html(
 								coinProfitPcnt.toFixed(2) + "&nbsp;%<br>"
 										+ nCoinProfit + "&nbsp;원");
+						
+						totalcoin += coinValue;
 
 					}
+//					console.log("totalcoin: "+ totalcoin);
+					$('#totalcoin').html(totalcoin.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+//					console.log("cash: "+$("#cash").val());
+					$('#totalAssets').html(($("#cash").val()+totalcoin).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
 				}
 			});
 };
